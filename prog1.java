90b7950df2186cb1d63bb377ace958896b27f37d
brrrdeesh <121388337+brrrdeesh@users.noreply.github.com>
2023-08-30 14:47:54 +0530

    Create prog1.java

---
+++ prog1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Team5 
+{
+  
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int min=(a<b)?a:b;
+        System.out.println("Min is "+min);
+      
+    }
+}
